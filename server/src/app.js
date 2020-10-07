fd392e41e03c257d043c2dbec2d1ea286aea296a
Mochiimaz <monmokasx@gmail.com>
2020-10-08 03:18:38 +0700

    Folder round2

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,49 @@
 let express = require('express')
-const app = express
+let bodyParser = require('body-Parser')
+const { json } = require('body-Parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req, res) {
+    console.log('hello - ' + req.params.person)
+    res.send('say hello with' + req.params.person)
+})
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน:' + req.params.userId)
+})
+
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+// create user
+app.post('/user/', function (req, res){
+    res.send('ทำการสร้างผู้ใช้งาน:' + JSON.stringify(req.body))
+})
+
+// edit user
+app.put('/user/:userId' , function (req, res){
+    res.send('ทำการแก้ไขผู้ใช้งาน:' + req.params.userId + ' : '
+    JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res){
+    res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' +
+    JSON.stringify(req.body)0)
+})
+let port = 8081
+app.listen(port, function() {
+    console.log('server running on ' + port)
+})
+// ล่าสุดหน้า 54 ทำความเข้าใจดึงฟังก์ชั่นอื่นที่นอกจาก status
